40de9533a1b32095b73e295c877237fc09d88f45
Ramy Medhat <ramymedhat97@gmail.com>
2024-06-15 17:21:46 +0300

    "Added updateCategory function to categoryService and updated categoryRoute to include PUT route for updating categories"

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
-
 const {
   getCategories,
   getCategory,
   createCategory,
+  updateCategory,
 } = require("../services/categoryService");
 
 const router = express.Router();
 
 router.route("/").get(getCategories).post(createCategory);
-router.route("/:id").get(getCategory);
+router.route("/:id").get(getCategory).put(updateCategory);
 
 module.exports = router;
