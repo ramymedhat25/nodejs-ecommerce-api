59cc4f32fb670f1f904fdbb3b7cd7b6399f5b65a
Ramy Medhat <ramymedhat97@gmail.com>
2024-06-23 18:46:16 +0300

    "Added conditional error handling for development environment"

--- middlewares/errorMiddlewares.js
+++ middlewares/errorMiddlewares.js
@@ -1,10 +1,16 @@
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
+  if (process.env.NODE_ENV == "development") {
+    sendErrorForDev(err, res);
+  }
+};
+const sendErrorForDev = (err, res) => {
+  return res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
 };
 
