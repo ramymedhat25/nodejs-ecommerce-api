c5d649dc43ce75017a937295d2e9714a1e8a0374
Ramy Medhat <ramymedhat97@gmail.com>
2024-07-25 18:19:08 +0300

    "Added productRoute to server.js and refactored categoryService.js"

---
+++ services/productService.js
@@ -0,0 +1,67 @@
+const slugify = require("slugify");
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/apiError");
+const Product = require("../models/productModel");
+
+// @desc GET List of products
+// @route GET /api/v1/products
+// @access public
+exports.getProducts = asyncHandler(async (req, res) => {
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 4;
+  const skip = (page - 1) * limit;
+  const products = await Product.find({}).skip(skip).limit(limit);
+  res.status(200).json({ results: products.length, page, data: products });
+});
+
+// @desc GET specify product by id
+// @route GET /api/v1/products/:id
+// @access public
+exports.getProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+  if (!product) {
+    return next(new ApiError(`No product found for this id ${id}`, 404));
+  }
+  res.status(200).json({ data: product });
+});
+
+// @desc Create product
+// @route POST /api/v1/products
+// @access private
+exports.createProduct = asyncHandler(async (req, res) => {
+  req.body.slug = slugify(req.body.title);
+  const product = await Product.create(req.body);
+  res.status(201).json({ data: product });
+});
+
+// @desc Update Specific product
+// @route PUT /api/v1/products/:id
+// @access private
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  req.body.slug = slugify(req.body.title);
+
+  const product = await Product.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+
+  if (!product) {
+    return next(new ApiError(`No product found for this id ${id}`, 404));
+  }
+  res.status(200).json({ data: product });
+});
+
+// @desc Delete Specific product
+// @route DELETE /api/v1/products/:id
+// @access private
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findByIdAndDelete(id);
+  if (!product) {
+    return next(new ApiError(`No product found for this id ${id}`, 404));
+  }
+  res.status(204).send();
+});
