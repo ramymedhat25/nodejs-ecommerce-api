3f94f7dfefc1938bc90c8d92c29f6e88fe5427cf
Ramy Medhat <ramymedhat97@gmail.com>
2024-07-26 19:19:14 +0300

    "Updated ESLint config, moved 'colors' package from dependencies to devDependencies, and refactored productService.js to implement filtering and pagination"

--- services/productService.js
+++ services/productService.js
@@ -7,13 +7,29 @@ const Product = require("../models/productModel");
 // @route GET /api/v1/products
 // @access public
 exports.getProducts = asyncHandler(async (req, res) => {
+  // 1) Filtering
+  const queryStringObj = { ...req.query };
+  const excludeFields = ["page", "sort", "limit", "fields"];
+  excludeFields.forEach((field) => delete queryStringObj[field]);
+  //Apply filteration using (gte|gt|lt|lte)
+  let queryStr = JSON.stringify(queryStringObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+
+  // 2) Pagination
   const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 4;
+  const limit = req.query.limit * 1 || 50;
   const skip = (page - 1) * limit;
-  const products = await Product.find({})
+  // Build Query
+  const mongooseQuery = Product.find(JSON.parse(queryStr))
     .skip(skip)
     .limit(limit)
     .populate({ path: "category", select: "name" });
+
+    //3) Sorting
+
+  //Execute Query
+  const products = await mongooseQuery;
+
   res.status(200).json({ results: products.length, page, data: products });
 });
 
