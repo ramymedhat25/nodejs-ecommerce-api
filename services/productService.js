555b27eb04650e4580b53ff6d3955d180a858ca2
Ramy Medhat <ramymedhat97@gmail.com>
2024-07-26 20:07:47 +0300

    "Refactored productService.js to use handlersFactory and updated ApiError class"

--- services/productService.js
+++ services/productService.js
@@ -1,104 +1,26 @@
-const slugify = require("slugify");
-const asyncHandler = require("express-async-handler");
-const ApiError = require("../utils/apiError");
+const factory = require("./handlersFactory");
 const Product = require("../models/productModel");
 
-// @desc GET List of products
-// @route GET /api/v1/products
-// @access public
-exports.getProducts = asyncHandler(async (req, res) => {
-  // 1) Filtering
-  const queryStringObj = { ...req.query };
-  const excludeFields = ["page", "sort", "limit", "fields"];
-  excludeFields.forEach((field) => delete queryStringObj[field]);
+// @desc    Get list of products
+// @route   GET /api/v1/products
+// @access  Public
+exports.getProducts = factory.getAll(Product, "Products");
 
-  // Apply filteration using (gte|gt|lt|lte)
-  let queryStr = JSON.stringify(queryStringObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+// @desc    Get specific product by id
+// @route   GET /api/v1/products/:id 
+// @access  Public
+exports.getProduct = factory.getOne(Product);
 
-  // 2) Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 50;
-  const skip = (page - 1) * limit;
+// @desc    Create product
+// @route   POST  /api/v1/products
+// @access  Private
+exports.createProduct = factory.createOne(Product);
+// @desc    Update specific product
+// @route   PUT /api/v1/products/:id
+// @access  Private
+exports.updateProduct = factory.updateOne(Product);
 
-  // Build Query
-  let mongooseQuery = Product.find(JSON.parse(queryStr))
-    .skip(skip)
-    .limit(limit)
-    .populate({ path: "category", select: "name" });
-
-  // 3) Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    mongooseQuery = mongooseQuery.sort(sortBy);
-  } else {
-    mongooseQuery = mongooseQuery.sort("-createdAt");
-  }
-
-  // 4) Field Control
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").map((field) => field.trim());
-    mongooseQuery = mongooseQuery.select(fields);
-  }
-
-  // Execute Query
-  const products = await mongooseQuery;
-
-  res.status(200).json({ results: products.length, page, data: products });
-});
-
-// @desc GET specify product by id
-// @route GET /api/v1/products/:id
-// @access public
-exports.getProduct = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-
-  const product = await Product.findById(id).populate({
-    path: "category",
-    select: "name",
-  });
-  if (!product) {
-    return next(new ApiError(`No product found for this id ${id}`, 404));
-  }
-  res.status(200).json({ data: product });
-});
-
-// @desc Create product
-// @route POST /api/v1/products
-// @access private
-exports.createProduct = asyncHandler(async (req, res) => {
-  req.body.slug = slugify(req.body.title);
-  const product = await Product.create(req.body);
-  res.status(201).json({ data: product });
-});
-
-// @desc Update Specific product
-// @route PUT /api/v1/products/:id
-// @access private
-exports.updateProduct = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  if (req.body.title) {
-    req.body.slug = slugify(req.body.title);
-  }
-  const product = await Product.findOneAndUpdate({ _id: id }, req.body, {
-    new: true,
-  });
-
-  if (!product) {
-    return next(new ApiError(`No product found for this id ${id}`, 404));
-  }
-  res.status(200).json({ data: product });
-});
-
-// @desc Delete Specific product
-// @route DELETE /api/v1/products/:id
-// @access private
-exports.deleteProduct = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-
-  const product = await Product.findByIdAndDelete(id);
-  if (!product) {
-    return next(new ApiError(`No product found for this id ${id}`, 404));
-  }
-  res.status(204).send();
-});
+// @desc    Delete specific product
+// @route   DELETE /api/v1/products/:id
+// @access  Private
+exports.deleteProduct = factory.deleteOne(Product);
